b61f7ea9d01cc995614f3d4ee008c2c9e81f2d74
Lady Meek <ladyjmeek@gmail.com>
2022-12-23 14:47:22 -0600

    Created Virtual Environment + Installed Packages + Started Project Folder

---
+++ heroes_villains_project/localsettings.py
@@ -0,0 +1,15 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-bsag&j9j^xs&78wih791dw)$10*h-ygp4ltr0!%+4)!f2^*kg^'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'heros_villains_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'password'
+    }
+}
